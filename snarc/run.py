3362205e36054bf86ccc0a9218da684fe7611cc3
James Knighton <iamknighton@gmail.com>
2020-02-14 00:44:38 -0800

    Just loading data.

--- snarc/run.py
+++ snarc/run.py
@@ -1,5 +1,9 @@
 from argparse import ArgumentParser
 from glob import glob
+import json
+import numpy as np
+
+from .solver import Solver
 
 
 def parse_flags():
@@ -8,12 +12,36 @@ def parse_flags():
     return x.parse_args()
 
 
+def load_split(xx):
+    pairs = []
+    for x in xx:
+        i = x['input']
+        o = x['output']
+        i = np.array(i, np.uint8)
+        o = np.array(o, np.uint8)
+        pairs.append((i, o))
+    return pairs
+
+
+def load_task(f):
+    x = json.load(open(f))
+    trains = load_split(x['train'])
+    tests = load_split(x['test'])
+    return trains, tests
+
+
+def run_task(solver, f):
+    trains, tests = load_task(f)
+    # TODO
+
+
 def main(flags):
+    solver = Solver()
     d = getattr(flags, 'in')
     ff = glob(d + '/*.json')
     ff.sort()
     for i, f in enumerate(ff):
-        print(i, f)
+        run_task(solver, f)
 
 
 if __name__ == '__main__':
